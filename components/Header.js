22825ca151dfb81c95a0b8c5bad1c5a1388a5a60
Ali Bulut <alibulut@yahoo.com>
2020-05-28 00:11:44 +0300

    the app re-designed for ios and android devices seperately by using platform api

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, Platform} from 'react-native'
 
 import TitleText from '../components/TitleText';
 import Colors from '../constants/colors';
 
 const Header = (props) => {
     return (
-        <View style={styles.header} >
-            <TitleText>{props.title}</TitleText>
+        //by using Platform.select() we are able to identify OS and styles which depend on it
+        <View style={{...styles.headerBase, ...Platform.select({ios: styles.headerIos, android: styles.headerAndroid})}} >
+            <TitleText style={styles.title}>{props.title}</TitleText>
         </View>     
     );
 };
 
 const styles = StyleSheet.create({
-    header: {
+    headerBase: {
         width:'100%',
         height:90,
         paddingTop:36,
-        backgroundColor: Colors.primary,
         alignItems: 'center',
         justifyContent: 'center'
+        //instead of using that we can create another styles for platforms
+        // backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white',
+        // borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
+        // borderBottomWidth: Platform.OS === 'ios' ? 1 : 0
+    },
+    headerIos:{
+        backgroundColor: 'white',
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1
+    },
+    headerAndroid:{
+        backgroundColor: Colors.primary
+    },
+    title:{
+        color:Platform.OS === 'ios' ? Colors.primary : 'white'
     }
 });
 
--- components/MainButton.ios.js
+++ components/MainButton.ios.js
@@ -3,6 +3,10 @@ import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 import Colors from '../constants/colors';
 
+//MainButton.ios and MainButton.android -> react native can understand those. So we dont need to check
+//whether the platform is Ios or android. React use components for appropriate platform. And also
+//when we import these files we should write ../components/MainButton !!! We dont need to write MainButton.ios or .android
+
 const MainButton = (props) => {
     return (
         <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
